b6584e7855c54925a1435eaf4c86087d9d881a1e
monotera <nelson.a.mosquera@gmail.com>
2021-02-18 11:23:11 -0500

    Creates CalculadoraNAMB

--- MyCalculator/src/mycalculator/Authors.java
+++ MyCalculator/src/mycalculator/Authors.java
@@ -12,5 +12,6 @@ package mycalculator;
 public class Authors {
     
     public static final String AMP = "Anabel Montero Posada";
+    public static final String NAMB = "Nelson Alejandro Mosquera Barrera";
     
 }
