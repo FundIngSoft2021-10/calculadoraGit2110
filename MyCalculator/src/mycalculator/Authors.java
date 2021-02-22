9ce425b35cb0c4f9ad7e361c18d98bfcffd5f1c4
DiegoSL <64794509+DiegoSL26@users.noreply.github.com>
2021-02-22 07:39:52 -0500

    Cambio DFSL

--- MyCalculator/src/mycalculator/Authors.java
+++ MyCalculator/src/mycalculator/Authors.java
@@ -15,6 +15,7 @@ public class Authors {
     public static final String NAMB = "Nelson Alejandro Mosquera Barrera";
     public static final String JPO = "Juan Pablo Ortiz";
     public static final String JPVF = "Juan Pablo Vera Fuentes";
+    public static final String DFSL = "Diego Fernando Sanchez Leyton";
     public static final String CJDCS = "Cristian Javier Da Camara Sousa";
 
     public static final String AIAA= "Abrahan Ignacio Ayala Acu�a";
